a1692b629606e08f2a19aa72bcf545f0d4e545b5
SJ <116865280+sj302003@users.noreply.github.com>
2025-04-17 21:11:06 +0530

    root and password DBconnection.java

--- src/dao/DBconnection.java
+++ src/dao/DBconnection.java
@@ -6,6 +6,6 @@ import java.sql.DriverManager;
 public class DBConnection {
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/snu_mart", "root", "your_password");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/snu_mart", "root", "mysql@123");
     }
 }
